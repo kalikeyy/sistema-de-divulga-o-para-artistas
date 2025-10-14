5cd8768d8bcec0ee6a5fbf4410d705d6c8bc89b3
kalikeyy <159036744+kalikeyy@users.noreply.github.com>
2025-10-13 21:10:03 -0300

    Update contato.py

--- python/contato.py
+++ python/contato.py
@@ -2,16 +2,16 @@ def sistema_contato():
     print("=== Fale Conosco ===")
    
     while True:
-        email = input("Digite seu e-mail (máx. 50 caracteres): ")
-        if len(email) <= 50:
+        email = input("Digite seu e-mail (máx. 150 caracteres): ")
+        if len(email) <= 150:
             break
-        print("E-mail inválido! Preencha corretamente e não ultrapasse 50 caracteres.")
+        print("E-mail inválido! Preencha corretamente e não ultrapasse 150 caracteres.")
 
     while True:
-        mensagem = input("Digite sua mensagem ou pergunta (máx. 200 caracteres): ")
-        if len(mensagem) <= 200:
+        mensagem = input("Digite sua mensagem ou pergunta (máx. 300 caracteres): ")
+        if len(mensagem) <= 300:
             break
-        print("Mensagem inválida! Preencha corretamente e não ultrapasse 200 caracteres.")
+        print("Mensagem inválida! Preencha corretamente e não ultrapasse 300 caracteres.")
 
     print("\nEnviando mensagem...")
     print("Mensagem enviada com sucesso! Aguardaremos seu contato.")
